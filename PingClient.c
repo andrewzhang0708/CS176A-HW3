d173af95166fc0d3936cf34fd383b62f421276ca
Andrew Zhang <chi_zhang952@csilvm-05.cs.ucsb.edu>
2025-11-08 18:08:58 -0800

    client

--- PingClient.c
+++ PingClient.c
@@ -1,84 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/time.h>
 
-// By Junjie Liu and Andrew Zhang, edited from HW2.
 
-void error(const char *);
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
 
-int main(int argc, char *argv[])
-{
-   int sock, n;
-   unsigned int length;
-   struct sockaddr_in server, from;
-   struct hostent *hp;
-   char buffer[256];
-   
-   if (argc != 3)
-   { 
-        printf("Usage: server port\n");
+double get_time_in_ms() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
+}
+
+int main(int argc, char *argv[]) {
+    int sock, portno, n;
+    struct sockaddr_in server, from;
+    struct hostent *hp;
+    socklen_t serverlen;
+    char buffer[1024];
+
+    const int TIMEOUT_SEC = 1;
+    const int NUM_PINGS = 10;
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
         exit(1);
-   }
-   sock= socket(AF_INET, SOCK_DGRAM, 0);
-   if (sock < 0)
-   {
+    }
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0)
         error("socket");
-   }
 
-   server.sin_family = AF_INET;
-   hp = gethostbyname(argv[1]);
-   if (hp==0)
-   {
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+
+    hp = gethostbyname(argv[1]);
+    if (hp == NULL)
         error("Unknown host");
-   }
 
-   bcopy((char *)hp->h_addr, 
-        (char *)&server.sin_addr,
-         hp->h_length);
-   server.sin_port = htons(atoi(argv[2]));
-   length=sizeof(struct sockaddr_in);
-   printf("Enter string: ");
-   bzero(buffer,256);
-   fgets(buffer,255,stdin);
-   n=sendto(sock,buffer,
-            strlen(buffer),0,(const struct sockaddr *)&server,length);
-   if (n < 0)
-   {
-        error("Sendto");
-   }
+    portno = atoi(argv[2]);
+    bzero((char *)&server, sizeof(server));
+    server.sin_family = AF_INET;
+    bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+    server.sin_port = htons(portno);
+    serverlen = sizeof(server);
 
-   while(1){
-        bzero(buffer, 256);
-        n = recvfrom(sock, buffer, 256, 0, (struct sockaddr *)&from, &length);
-        if(n < 0) error("recvfrom");
-    
-        printf("From server: ");
-        fflush(stdout);
-        write(1, buffer, n);
+    // printf("PING %s:%d with %d packets:\n", argv[1], portno, NUM_PINGS);
 
-        buffer[n] = '\0';
-        if(n > 0 && buffer[n-1] == '\n'){
-            buffer[n-1] = '\0';
+    double rtts[NUM_PINGS];
+    int received = 0;
+
+    for (int i = 0; i < NUM_PINGS; i++) {
+        double send_time = get_time_in_ms();
+
+        sprintf(buffer, "PING %d %f", i + 1, send_time);
+
+        n = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&server, serverlen);
+        if (n < 0)
+            error("sendto");
+
+        bzero(buffer, sizeof(buffer));
+        n = recvfrom(sock, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&from, &serverlen);
+        double recv_time = get_time_in_ms();
+
+        if (n < 0) {
+            printf("Request timeout for seq #%d\n", i + 1);
+            rtts[i] = -1;
+        } else {
+            buffer[n] = '\0';
+            double rtt = recv_time - send_time;
+            rtts[i] = rtt;
+            received++;
+            printf("PING reply from %s: seq=%d time=%.3f ms\n",
+                   inet_ntoa(from.sin_addr), i + 1, rtt);
         }
+        sleep(1);
+    }
 
-        if(strncmp(buffer, "Sorry", 5) == 0 || (strlen(buffer) == 1 && buffer[0] >= '0' && buffer[0] <= '9')){
-            exit(0);
+    double sum = 0, min = 1e9, max = -1;
+    for (int i = 0; i < NUM_PINGS; i++) {
+        if (rtts[i] > 0) {
+            sum += rtts[i];
+            if (rtts[i] < min) min = rtts[i];
+            if (rtts[i] > max) max = rtts[i];
         }
     }
 
-    printf("\n");
-   close(sock);
-   return 0;
-}
+    printf("\n--- %s ping statistics ---\n", argv[1]);
+    printf("%d packets transmitted, %d received, %.0f%% packet loss\n",
+           NUM_PINGS, received, 100.0 * (NUM_PINGS - received) / NUM_PINGS);
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
+    if (received > 0) {
+        printf("rtt min/avg/max = %.3f/%.3f/%.3f ms\n",
+               min, sum / received, max);
+    }
+
+    close(sock);
+    return 0;
 }
