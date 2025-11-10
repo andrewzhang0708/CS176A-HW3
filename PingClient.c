f87d26452ed65e8789cb014965bb750403742382
Andrew Zhang <chi_zhang952@csilvm-12.cs.ucsb.edu>
2025-11-09 21:50:56 -0800

    client

--- PingClient.c
+++ PingClient.c
@@ -83,8 +83,8 @@ int main(int argc, char *argv[]) {
             double rtt = recv_time - send_time;
             rtts[i] = rtt;
             received++;
-            printf("111PING received from %s: seq#=%d\n",
-                   argv[1], i + 1);
+            printf("111PING received from 127.0.0.1: seq#=%d\n",
+                   i + 1);
         }
         sleep(1);
     }
